943df60098686e1613d7eebb33306041475535a9
Stefano <stefanonicodemo@libero.it>
2022-07-16 14:36:42 +0200

    Recovered some features

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -6,24 +6,80 @@ using UnityEngine;
 public class CameraControl : MonoBehaviour
 {
 
-    [SerializeField] float cameraSpeed;
+    [SerializeField] float cameraWasdSpeed;
+    [SerializeField] float dragSpeed;
+    Vector2 mouseClickPos;
+    Vector2 mouseCurrentPos;
+    bool panning = false;
+
+
+    [SerializeField] float widthBorderSpeed;
+    [SerializeField] float heightBorderSpeed;
+    float leftBorder = Screen.width * 0.05f;
+    float rightBorder = Screen.width * 0.95f;
+    float topBorder = Screen.height * 0.95f;
+    float bottomBorder = Screen.height * 0.05f;
+
+    void Start()
+    {
+    }
 
     void Update()
     {
         WasdMovement();
-        MouseDrag();
+
+        Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        BorderScreenMovement(mousePosition);
+
+        // When LMB clicked get mouse click position and set panning to true
+        if (Input.GetKeyDown(KeyCode.Mouse0) && !panning)
+        {
+            mouseClickPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            panning = true;
+        }
+        // If LMB is already clicked, move the camera following the mouse position update
+        if (panning)
+        {
+            mouseCurrentPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            var distance = mouseCurrentPos - mouseClickPos;
+            transform.position += new Vector3(-distance.x, -distance.y, 0);
+        }
+
+        // If LMB is released, stop moving the camera
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+            panning = false;
+
     }
 
-    private void MouseDrag()
+    private void BorderScreenMovement(Vector2 mousePosition)
     {
-        Vector3 lastPosition = transform.position;
-    
+
+
+        if (mousePosition.x < leftBorder)
+        {
+            transform.position = new Vector3(transform.position.x - widthBorderSpeed, transform.position.y, transform.position.z);
+        }
+        else if (mousePosition.x > rightBorder)
+        {
+            transform.position = new Vector3(transform.position.x + widthBorderSpeed, transform.position.y, transform.position.z);
+        }
+
+        if (mousePosition.y < bottomBorder)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - heightBorderSpeed, transform.position.z);
+        }
+        else if (mousePosition.y > topBorder)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + heightBorderSpeed, transform.position.z);
+        }
+
     }
 
+    //Si occupa del movimento con wasd
     private void WasdMovement()
     {
-        float xAxisValue = Input.GetAxis("Horizontal") * cameraSpeed;
-        float zAxisValue = Input.GetAxis("Vertical") * cameraSpeed;
+        float xAxisValue = Input.GetAxis("Horizontal") * cameraWasdSpeed;
+        float zAxisValue = Input.GetAxis("Vertical") * cameraWasdSpeed;
 
         transform.position = new Vector3(transform.position.x + xAxisValue, transform.position.y + zAxisValue, transform.position.z);
     }
