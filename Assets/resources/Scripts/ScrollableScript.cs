95e3793efe0d4789c7e02f5c4680d06b52d34509
Stefano <stefanonicodemo@libero.it>
2022-07-16 16:41:34 +0200

    Passo Game object

--- Assets/resources/Scripts/ScrollableScript.cs
+++ Assets/resources/Scripts/ScrollableScript.cs
@@ -24,12 +24,12 @@ public class ScrollableScript : MonoBehaviour
         {
             case "Smith":
                 gridControllerScript.TestCall("Sono in Smith");
-                gridControllerScript.buildingTile = (Building) Resources.Load("Prefabs/Blacksmith", typeof(GameObject));
+                gridControllerScript.buildingObject = (GameObject) Resources.Load("Prefabs/Blacksmith", typeof(GameObject));
                 break;
 
             case "Farm":
                 gridControllerScript.TestCall("Sono in Farm");
-                gridControllerScript.buildingTile = (Building) Resources.Load("Prefabs/Farm", typeof(GameObject));
+                gridControllerScript.buildingObject = (GameObject) Resources.Load("Prefabs/Farm", typeof(GameObject));
                 break;
 
             default:
